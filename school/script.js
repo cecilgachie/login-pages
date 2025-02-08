80c610005e63107771424631bd054073be404226
cecilgachie <159792219+cecilgachie@users.noreply.github.com>
2025-02-08 12:59:05 +0300

    Delete school directory

--- school/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('flipToSignUp').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('.card').classList.add('flipped');
-});
-
-document.getElementById('flipToSignIn').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('.card').classList.remove('flipped');
-});
